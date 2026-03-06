3842252e57b4278f9599962d0f74e84cbdfbeef5
Augusto Altmayer <augustoaltmayer@email.com>
2026-03-06 17:47:22 -0500

    Corrigir tema VSCode dark, cores e animações

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
   return (
     <ErrorBoundary>
       <ThemeProvider
-        defaultTheme="light"
+        defaultTheme="dark"
       >
         <TooltipProvider>
           <Toaster />
