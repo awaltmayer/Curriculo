06092ef85d47c7f65ca908b03929ad3f4ec849ac
Augusto <augustowolfartaltmayer30@gmail.com>
2026-03-09 23:40:38 +0000

    feat: adicionar peixes nadando no plano de fundo
    - Criado componente SwimmingFish com animações fluidas
    - Peixes azuis estilo koi com movimento natural
    - Animações de cauda, barbatanas e corpo
    - 8-12 peixes com trajetórias variadas
    - Não interativo, apenas decorativo no background

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
+import SwimmingFish from "./components/SwimmingFish";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 
@@ -22,6 +23,7 @@ function App() {
     <ErrorBoundary>
       <ThemeProvider defaultTheme="dark">
         <TooltipProvider>
+          <SwimmingFish />
           <Toaster />
           <Router />
         </TooltipProvider>
